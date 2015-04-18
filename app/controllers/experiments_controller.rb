52326a66679cdb37e1a909bc130bc3685c05e916
Marko Lovic <markolovic@hotmail.com>
2015-04-18 13:08:11 +0200

    Create views for experiment model

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,7 +1,11 @@
 class ExperimentsController < ApplicationController
 
   def index
-    
+    @experiments = Experiment.all
+  end
+
+  def show
+    @experiment = Experiment.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'experiments#index'
 
+  resources :experiments
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
