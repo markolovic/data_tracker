71f91b5e0a5d128afcf5b0438e5c1735d35b201c
Marko Lovic <markolovic@hotmail.com>
2015-04-19 16:57:13 +0200

    Fix gon arrays

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -11,15 +11,30 @@ class ExperimentsController < ApplicationController
   def show
     @experiments = Experiment.all
     @experiment = Experiment.find(params[:id])
+
+    #ind_vars = @experiment.independent_vars.
+    ind_vars = Array.new
+    dep_vars = []
+    measurements = []
+
+    @experiment.independent_vars.all.each do |var|
+      var.measurements.all.each do |m|
+        measurements << m.value
+      end
+      ind_vars << measurements
+      measurements = []
+    end
+    @experiment.dependent_vars.all.each do |var|
+      var.measurements.all.each do |m|
+        measurements << m.value
+      end
+      dep_vars << measurements
+      measurements = []
+    end
+
     gon.experiment = Experiment.find(params[:id])
-    gon.ind_vars = @experiment.independent_vars
-    gon.dep_vars = @experiment.dependent_vars
-    @experiment.independent_vars.all.each_with_index do |var, counter|
-      gon.ind_vars[counter].measurements = var.measurements.all
-    end
-    @experiment.dependent_vars.all.each_with_index do |var, counter|
-      gon.dep_vars[counter].measurements = var.measurements.all
-    end
+    gon.ind_vars = ind_vars
+    gon.dep_vars = dep_vars
 
     #gon.experiments.each_with_index do |e, counter|
       #gon.experiments[counter].independent_vars = e.independent_vars
