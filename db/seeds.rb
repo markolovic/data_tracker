4ce4c1480da45dc4c127edc7795a0830d0fba1b4
Pedro Morales Rodriguez <Fyx@MacBook-Pro-de-Pedro.local>
2015-04-19 16:31:43 +0200

    GET IN HERE

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,13 @@ Experiment.delete_all
 IndependentVar.delete_all
 Measurement.delete_all
 
-sleep = Experiment.create(name: "Sleep quality")
+sleep = Experiment.create(name: "Curva de balmer")
 running = Experiment.create(name: "Diet and Running")
 
 # Ind vars
-time =  sleep.independent_vars.create(name: "Wake up time", mtype: "number")
+time =  sleep.independent_vars.create(name: "Cervezas", mtype: "number")
 #sleep.independent_vars << IndependentVar.create(name: "Wake up timee")
-tired = sleep.independent_vars.create(name: "Tired upong waking", mtype: "number")
+tired = sleep.independent_vars.create(name: "Eficiencia al programar", mtype: "number")
 
 # Dep vars
 dinner =  sleep.dependent_vars.create(name: "Dinner time", mtype: "number")
