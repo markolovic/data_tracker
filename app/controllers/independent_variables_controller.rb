c9ff8cff287b39bb4123e35ae44784271120b607
Marko Lovic <markolovic@hotmail.com>
2015-04-19 17:40:24 +0200

    Create reporting

--- app/controllers/independent_variables_controller.rb
+++ app/controllers/independent_variables_controller.rb
@@ -1,6 +1,18 @@
 class IndependentVariablesController < ApplicationController
 
   def edit
-    @ind_var = IndependentVar.id(params[:id])
+    #@ind_var = IndependentVar.id(params[:id])
   end
+
+  def update
+    @var = IndependentVar.find(params[:id])
+    @var.measurements << Measurement.create(value: params[:value])
+    @var.save
+    redirect_to @var.experiment
+  end
+  private
+    #def var_params
+      #params.require(:independent_var).permit(:name, :credits)
+    #end
 end
+
